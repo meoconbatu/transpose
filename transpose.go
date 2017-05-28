edfe8b7f12963efa304b0a22d177f8588ccb6c52
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-05-28 22:36:31 +0700

    first commit

--- transpose.go
+++ transpose.go
@@ -6,15 +6,30 @@ func Transpose(in []string) []string {
 	if len(in) == 0 {
 		return make([]string, 0)
 	}
-	ou := make([][]rune, 1000)
+	ou := make([][]rune, 0)
+	lenOu := len(ou)
 	for i, str := range in {
 		for j, r := range str {
-			ou[j][i] = r
+			if j >= lenOu {
+				x := make([]rune, i+1)
+				x[i] = r
+				for z := 0; z < i; z++ {
+					x[z] = ' '
+				}
+				ou = append(ou, x)
+				lenOu = len(ou)
+			} else {
+				for z := len(ou[j]); z < i; z++ {
+					ou[j] = append(ou[j], ' ')
+				}
+				ou[j] = append(ou[j], r)
+			}
+
 		}
 	}
 	output := make([]string, len(ou))
-	for i, o := range ou {
-		output[i] = string(o)
+	for i, str := range ou {
+		output[i] = string(str)
 	}
 	return output
 }
